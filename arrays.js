0065bb625f2a319f7a12443199dc616b5c162ff8
skylerwphillipstx@gmail.com Phillips <skylerwphillipstx@gmail.com>
2018-07-30 21:21:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,7 +21,8 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array, element){
-  return array.shift(element)
+   array.shift(element)
+   return array
 }
 
 
