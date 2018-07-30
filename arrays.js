e1fd2394bab5b18c7427c1dcbb0c37214bff8854
skylerwphillipstx@gmail.com Phillips <skylerwphillipstx@gmail.com>
2018-07-30 21:21:14 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,42 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles',]
+var mars = 'mars bar'
+function addElementToBeginningOfArray(array, element) {
+  return [ element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element)
+}
+
+
+function addElementToEndOfArray(array, element) {
+ return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array = array.push(element)
+}
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array, element){
+  return array.shift(element)
+}
+
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  return array.pop()
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
+
+
+
+
