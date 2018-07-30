04845764ad7d002ceae74f5d02d409b66c28301e
skylerwphillipstx@gmail.com Phillips <skylerwphillipstx@gmail.com>
2018-07-30 21:21:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,8 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
-  return array.pop()
+  array.pop()
+  return array
 }
 
 function removeElementFromEndOfArray(array) {
